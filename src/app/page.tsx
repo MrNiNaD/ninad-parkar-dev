847182ec2b9837dd8124a5ab6062ad6748104f4e
MrNiNaD <ninadparkar273@gmail.com>
2024-05-26 17:20:13 +0530

    feat: title section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/component/Header";
+import Title from "@/component/Title";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Title />
     </>
   );
 }
---
+++ src/component/Title/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import style from "@/app/page.module.css";
+import Ninad from "@/assets/image/ninad.jpg";
+import Image from "next/image";
+
+const Title = () => {
+  return (
+    <section className={style.myTitle}>
+      <div className={style.myDetails}>
+        <h2 className={style.heading2}>Hello, I'm Ninad,</h2>
+        <div className={style.designationContainer}>
+          <span className={`${style.heading2} ${style.designation}`}>
+            Software
+          </span>
+          <span className={`${style.heading2} ${style.designation}`}>
+            Engineer
+          </span>
+        </div>
+        <span className={style.heading2}>Based in Mumbai, Maharashtra</span>
+        <div className={style.btnContainer}>
+          <span className={style.btn}>
+            <a
+              target="_blank"
+              href="https://drive.google.com/file/d/1F-HJxohECyOeYFZB3PVc7q9Ivf9vGpSr/view?usp=sharing"
+            >
+              Resume
+            </a>
+            <span className={style.btnOverlay} />
+          </span>
+        </div>
+      </div>
+      <div className={style.imageSection}>
+        <Image src={Ninad} alt="Ninad Parkar" />
+      </div>
+    </section>
+  );
+};
+
+export default Title;
