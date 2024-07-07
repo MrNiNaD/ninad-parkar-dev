2bcbb7bf65b1aae4566960f3fe8c0aadee136610
MrNiNaD <ninadparkar273@gmail.com>
2024-07-07 17:43:18 +0530

    feat: car dodging game added in project

--- src/component/Project/index.tsx
+++ src/component/Project/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import style from "@/app/page.module.css";
+import Image from "next/image";
+import CarDodging2 from "@/assets/image/car-doging-2.gif";
+import CarDodging from "@/assets/image/car-doging.gif";
 
 const Project = () => {
+  const config = [
+    {
+      projectTitle: "Car Dodging",
+      previewImg: [
+        { src: CarDodging, alt: "Car Dodging Pic 1" },
+        { src: CarDodging2, alt: "Car Dodging Pic 2" },
+      ],
+      link: "https://mrninad.github.io/car-game/",
+    },
+  ];
+
   return (
     <section className={style.aboutSectionStyle} id="project">
       <h3
@@ -9,7 +23,43 @@ const Project = () => {
       >
         project.
       </h3>
-      <p className={`${style.paragraph} ${style.aboutText}`}>Coming soon...</p>
+
+      <ul className={style.projectListing}>
+        {config.map((eachConfig, index) => {
+          return (
+            <li key={index}>
+              <span
+                className={`${style.paragraph} ${style.aboutText} ${style.projectTitle}`}
+              >
+                {eachConfig.projectTitle}
+              </span>
+              <div className={style.previewImgContainer}>
+                {Array.isArray(eachConfig.previewImg) &&
+                  eachConfig.previewImg.map((eachImgConfig, index) => {
+                    return (
+                      <Image
+                        key={index}
+                        width={200}
+                        src={eachImgConfig.src}
+                        alt={eachImgConfig.alt}
+                      />
+                    );
+                  })}
+              </div>
+              <div className={style.projectLinkContainer}>
+                <span
+                  className={`${style.paragraph} ${style.aboutText} ${style.projectLink}`}
+                >
+                  Link:{" "}
+                  <a target="_blank" href={eachConfig?.link}>
+                    {eachConfig?.link}
+                  </a>
+                </span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 };
